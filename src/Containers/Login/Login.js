d18b661048d9e23fa6b67948b999b8ecf71ee793
Ahmed Tarek <ahmed-tarek94@hotmail.com>
2019-05-24 14:47:51 +0200

    Added event listeners

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -3,63 +3,82 @@ import './Login.css'
 
 
 class Login extends Component {
+    state = {
+        username : '',
+        password : '',
+        error : null
+    }
+
+    setUsernameState = (event) => {
+        this.setState({
+            username : event.target.value
+        })
+    }
+
+    setPasswordState = (event) => {
+        this.setState({
+            password : event.target.value
+        })
+    }
+
+
     render() {
         return (
-            <div class="mid-class">
-                <div class="art-right-w3ls">
+            <div className="mid-class">
+                <div className="art-right-w3ls">
                     <h2>Sign In</h2>
-                    <div class="main">
-                        <div class="form-left-to-w3l">
-                            <input type="text" name="name" placeholder="Username" required="" />
+                    <div className="main">
+                        <div className="form-left-to-w3l">
+                            <input type="text" name="name" placeholder="Username" required="" onChange={(event) => this.setUsernameState(event)}/>
                         </div>
-                        <div class="form-left-to-w3l ">
-                            <input type="password" name="password" placeholder="Password" required="" />
-                            <div class="clear"></div>
+                        <div className="form-left-to-w3l ">
+                            <input type="password" name="password" placeholder="Password" required="" onChange = {(event) => this.setPasswordState(event) } />
+                            <div className="clear"></div>
                         </div>
                     </div>
-                    <div class="left-side-forget">
-                        <input type="checkbox" class="checked" />
-                        <span class="remenber-me">Remember me </span>
+                    <div className="left-side-forget">
+                        <input type="checkbox" className="checked" />
+                        <span className="remenber-me">Remember me </span>
                     </div>
-                    <div class="right-side-forget">
-                        <a href="#" class="for">Forgot password...?</a>
+                    <div className="right-side-forget">
+                        <a href="#" className="for">Forgot password...?</a>
                     </div>
-                    <div class="clear"></div>
-                    <div class="btnn">
+                    <div className="clear"></div>
+                    <div className="btnn">
                         <button type="submit">Sign In</button>
                     </div>
-                    <div class="w3layouts_more-buttn">
+                    <div className="w3layouts_more-buttn">
                         <h3>Don't Have an account..?
                   <a href="#content1">Sign Up Here
                   </a>
                         </h3>
                     </div>
-                    <div id="content1" class="popup-effect">
-                        <div class="popup">
-                            <div class="letter-w3ls">
+                    <div id="content1" className="popup-effect">
+                        <div className="popup">
+                            <div className="letter-w3ls">
                                 <form >
-                                    <div class="form-left-to-w3l">
+                                    <div className="form-left-to-w3l">
                                         <input type="text" name="name" placeholder="Username" required="" />
                                     </div>
-                                    <div class="form-left-to-w3l">
+                                    <div className="form-left-to-w3l">
                                         <input type="text" name="name" placeholder="Phone" required="" />
                                     </div>
-                                    <div class="form-left-to-w3l">
+                                    <div className="form-left-to-w3l">
                                         <input type="email" name="email" placeholder="Email" required="" />
                                     </div>
-                                    <div class="form-left-to-w3l">
+                                    <div className="form-left-to-w3l">
                                         <input type="password" name="password" placeholder="Password" required="" />
                                     </div>
-                                    <div class="form-left-to-w3l margin-zero">
+                                    <div className="form-left-to-w3l margin-zero">
                                         <input type="password" name="password" placeholder="Confirm Password" required="" />
                                     </div>
-                                    <div class="btnn">
+                                    <div className="btnn">
                                         <button type="submit">Sign Up</button>
                                     </div>
                                 </form>
-                                <div class="clear"></div>
+                                <div className="clear"></div>
                             </div>
-                            <a class="close" href="#">&times;</a>
+                            <a className="close" href="#">&times;</a>
                         </div>
                     </div>
                 </div>
